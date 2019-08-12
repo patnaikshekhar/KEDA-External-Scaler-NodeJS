9c400a74560966440bc5e58c6322c5c312fb393e
Shekhar Patnaik <patnaikshekhar@gmail.com>
2019-08-12 10:38:28 +0100

    Added remanining services

--- services.js
+++ services.js
@@ -1,44 +1,102 @@
 const MongoClient = require('mongodb').MongoClient
 const grpc = require('grpc')
 
+const METRIC_NAME = 'MongoCollectionLength'
+
 const scaledObjects = {}
 
+
 function New(request, callback) {
-    console.log('In New', request)
-    let errorMessage
-    if ('metadata' in request.request) {
+    const requiredFields = ['connectionString', 'db', 'collectionName', 'length']
+    const missingFields = requiredFields.filter(
+        field => !(field in request.request.metadata))
 
-    } else {
-        console.log('Here')
-        errorMessage = 'No Metadata present'
-    }
-
-    if (errorMessage) {
+    if (missingFields.length > 0) {
+        const errorMessage = `Missing ${missingFields.join(', ')} in metadata`
         callback({
             code: 400,
             message: errorMessage,
             status: grpc.status.INVALID_ARGUMENT
         })
     } else {
+        const name = getScaledObjectName(request.request.scaledObjectRef)    
+        scaledObjects[name] = request.request.metadata
         callback(null, {})
     }
-    
 }
 
-function IsActive() {
-
+function IsActive(request, callback) {
+    const name = getScaledObjectName(request.request)
+    const so = scaledObjects[name] 
+    getCollectionSize(so.connectionString, so.db, so.collectionName)
+        .then(length => {
+            callback(null, {
+                result: length > 0
+            })
+        })
+        .catch(e => {
+            console.error(e)
+            callback({
+                code: 500,
+                message: e.toString(),
+                status: grpc.status.INTERNAL
+            })
+        })
 }
 
-function GetMetricSpec() {
+function GetMetricSpec(request, callback) {
+    const name = getScaledObjectName(request.request)
+    const so = scaledObjects[name] 
 
+    callback(null, {
+        metricSpecs: [{
+            metricName: METRIC_NAME,
+            targetSize: so.length
+        }]
+    })
 }
 
-function GetMetrics() {
-
+function GetMetrics(request, callback) {
+    const name = getScaledObjectName(request.request.scaledObjectRef)
+    const so = scaledObjects[name] 
+    getCollectionSize(so.connectionString, so.db, so.collectionName)
+        .then(length => {
+            callback(null, {
+                metricValues: [{
+                    metricName: METRIC_NAME,
+                    metricValue: length
+                }]
+            })
+        })
+        .catch(e => {
+            console.error(e)
+            callback({
+                code: 500,
+                message: e.toString(),
+                status: grpc.status.INTERNAL
+            })
+        })
 }
 
-function Close() {
+function Close(request, callback) {
+    const name = getScaledObjectName(request.request)
+    delete scaledObjects[name]
+    callback(null, {})
+}
 
+function getScaledObjectName(scaledObjectRef) {
+    const scaledObjectName = scaledObjectRef.name
+    const scaledObjectNamespace = scaledObjectRef.namespace
+    const scaledObject = `${scaledObjectNamespace}/${scaledObjectName}`
+
+    return scaledObject
+}
+
+async function getCollectionSize(connectionString, dbName, collectionName) {
+    const connection = await MongoClient.connect(connectionString)
+    const count = await connection.db(dbName).collection(collectionName).count()
+    await connection.close()
+    return count
 }
 
 module.exports = {
